f36107b464e56d8fde9ceb78b2104e82330a7f3a
Stas <100081623+9Get@users.noreply.github.com>
2024-01-26 18:33:56 +0200

    LR 1

---
+++ IV course/ASP.NET/LR1/Entities/Company.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LR1.Entities
+{
+	public class Company
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public int BranchesNumber { get; set; }
+		public Company()
+		{
+			Name = string.Empty;
+			Address = string.Empty;
+			BranchesNumber = 0;
+		}
+		public Company(string name, string address, int branchesNumber)
+		{
+			Name = name;
+			Address = address;
+			BranchesNumber = branchesNumber;
+		}
+		public override string ToString()
+		{
+			return $"Name: {Name};\nAddress: {Address};\nNumber of branches: {BranchesNumber}.\n";
+		}
+	}
+}
